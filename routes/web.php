bd8f3c7c114b9faa5959f9272c554012ddfc0fdf
Renato Andrade <renatorobot@outlook.com.br>
2021-01-20 10:01:55 -0300

    Agrupamento de rotas

--- routes/web.php
+++ routes/web.php
@@ -41,3 +41,29 @@ Route::get('/rotacomregras/{nome}/{n}', function($nome, $n) {
     }
     
 })->where('nome', '[a-zA-Z]+')->where('n', '[0-9]+');
+
+
+
+Route::prefix('/app')->group(function(){
+
+    Route::get('/', function() {
+    
+       return view('app');
+        
+    });
+
+    Route::get('/user', function() {
+    
+        return view('user');
+         
+     });
+
+
+     Route::get('profile/', function() {
+    
+        return view('profile');
+         
+     });
+
+
+});
