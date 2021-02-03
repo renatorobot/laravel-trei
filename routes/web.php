ef83cda4e43b0c0f761a77123019069d4adc765a
Renato Andrade <renatorobot@outlook.com.br>
2021-02-03 09:06:10 -0300

    Associando rotas ao controlador

--- routes/web.php
+++ routes/web.php
@@ -13,3 +13,6 @@ Route::get('/nome', "MeuControlador@getNome");
 Route::get('/idade', "MeuControlador@getIdade");
 
 Route::get('/multiplicar/{n1}/{n2}', "MeuControlador@multiplicar");
+
+
+Route::resource('user', 'ClienteControlador');
